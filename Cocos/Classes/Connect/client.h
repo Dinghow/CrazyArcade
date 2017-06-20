053e4a1a9a31d0c987edcc76ca9fb6713b4c9041
Dinghow <DinghowYang@gmail.com>
2017-06-21 00:17:28 +0800

    Function Update0.9.5(Repair the bug of server,add auto-close server function)

--- Cocos/Classes/Connect/client.h
+++
@@ -1,9 +0,0 @@
-#ifndef __CLIENT_H__
-#define __CLIENT_H__
-
-void message();
-void choose();
-void Client();
-
-#endif // __CLIENT_H__
-
--- Cocos/Classes/GamePlay/RoleMove.cpp
+++
@@ -1,8 +0,0 @@
-#include "PlayScene.h"
-#include"Player.h"
-
-
-
-
-
-
